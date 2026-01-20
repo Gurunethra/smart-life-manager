cff44956a53747b14227e0d521fddb51e5549147
guru nethra <gurunethraa@gmail.com>
2026-01-20 20:33:33 +0530

    added some files

---
+++ js/main.js
@@ -0,0 +1,7 @@
+document.getElementById("tasksBtn").addEventListener("click", loadTasks);
+document.getElementById("expenseBtn").addEventListener("click", loadExpense);
+document.getElementById("weatherBtn").addEventListener("click", loadWeather);
+document.getElementById("dashboardBtn").addEventListener("click", loadDashboard);
+
+// Default load
+loadTasks();
